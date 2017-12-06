0228c20248e345f6a1111198aab12a28c75aabe4
Alex Breat <HTC.hlebowitsc@gmail.com>
2017-12-06 09:02:03 +0200

    Create project

---
+++ Example1/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+		public static void main(String[] args) {
+				System.out.println("Hello World!");
+				// Два слеша блокируют коментарий в одну строку
+				/* Слеш с звездочкой блокирует коментарий заключенный между ними
+				 в независимости от количества написанных сток
+				 */
+				System.out.println("123456"); // Цифры как текст
+				System.out.println(1234);// Цифры как цифры
+				System.out.println("Line1\nLine2\n\tLine3\n\t\tLine4");
+				System.out.println(10+50);
+				System.out.println(10*20);
+				System.out.println(10*20*10);
+			}
+	}
