81f4a02de6be599486ed848d7c55c3a5a12afda6
Alex Breat <htc.hlebowitsc@gmail.com>
2017-12-12 02:17:45 +0200

    Lesson 2

---
+++ Example1/src/Example.java
@@ -0,0 +1,12 @@
+public class Example
+{
+	public static void main (String[] argm) {
+		int a = 0;
+		int b = 0;
+		System.out.println(a++);
+		System.out.println(++a);
+		b=a;
+		System.out.println(b);
+		System.out.println(--b);
+	}
+}
